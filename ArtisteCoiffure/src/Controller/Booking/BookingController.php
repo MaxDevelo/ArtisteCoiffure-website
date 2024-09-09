fa9df42291b11f7f02b5a50f74d791bf4ff214da
MaxDevelo <maxime.wawrzy@gmail.com>
2024-09-09 20:18:18 +0200

    feat(home): add path if booking page in footer and header

--- ArtisteCoiffure/src/Controller/Booking/BookingController.php
+++ ArtisteCoiffure/src/Controller/Booking/BookingController.php
@@ -2,14 +2,16 @@
 
 declare(strict_types= 1);
 
-namespace App\Controller;
+namespace App\Controller\Booking;
+
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class BookingController extends AbstractController
 {
     #[Route(path:"/reservation_en_ligne", name:"app_booking_show")]
-    public function show()
+    public function show(): Response
     {
         return $this->render("pages/booking.html.twig");
     }
--- ArtisteCoiffure/src/Controller/HomeController.php
+++ ArtisteCoiffure/src/Controller/HomeController.php
@@ -3,14 +3,16 @@
 declare(strict_types= 1);
 
 namespace App\Controller;
+
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route(path:"/", name:"app_home")]
 class HomeController extends AbstractController
 {
     #[Route(path:"", name:"app_home_index")]
-    public function index()
+    public function index(): Response
     {
         return $this->render("home.html.twig");
     }
