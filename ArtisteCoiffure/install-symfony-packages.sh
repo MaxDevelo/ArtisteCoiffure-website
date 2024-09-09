904cd22ea64f530c472387632f51e82a5f4cb053
MaxDevelo <maxime.wawrzy@gmail.com>
2024-09-09 18:10:58 +0200

    feat: init project

---
+++ ArtisteCoiffure/install-symfony-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+composer require debug
+composer require twig
+composer require cs-fixer
+composer require asset-mapper
+composer require asset
+composer require doctrine
+composer require symfony/webpack-encore-bundle
+npm install -D tailwindcss postcss postcss-loader autoprefixer
+npx tailwindcss init -p
